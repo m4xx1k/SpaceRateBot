35a8aca75def73c72707e40e8e5e914881f649f7
max fabin <maxikfabin@gmail.com>
2023-06-23 00:25:40 +0300

    .

---
+++ index.js
@@ -0,0 +1,27 @@
+const Telegraf = require('telegraf')
+
+const BOT_TOKEN = '6143502881:AAEQmvcZkDavYqOjfvvjXl7tpWLskmI7OEc'
+const URL = ''
+const PORT = process.env.PORT || 3000
+
+const bot = new Telegraf(BOT_TOKEN)
+
+// Встановлюємо команду start для генерації deep link
+bot.start((ctx) => {
+    ctx.reply(`Вітаю! Спробуйте deep link: https://t.me/${ctx.botInfo.username}?start=test_payload`)
+})
+
+// Обробка deep linking.
+// Коли користувач натискає на посилання, бот отримає команду /start з вказаним payload.
+bot.command('start', (ctx) => {
+    const payload = ctx.message.text.split(' ')[1]
+    ctx.reply(`Deep link payload: ${payload}`)
+})
+
+// Тут ви можете додати інші обробники команд і повідомлень.
+
+// Налаштування вебхука
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`)
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
+
+console.log(`Bot is running on ${PORT}`)
