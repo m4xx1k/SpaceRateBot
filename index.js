f4e26c7ec12db63502e650ccd0da40f254658726
max fabin <maxikfabin@gmail.com>
2023-06-23 00:46:56 +0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 
-const BOT_TOKEN = '6143502881:AAEQmvcZkDavYqOjfvvjXl7tpWLskmI7OEc'
-const domain = 'https://fine-plum-crab-ring.cyclic.app'
-const port = 5000 || process.env.PORT
-import { Telegraf } from "telegraf";
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const TOKEN = '6143502881:AAEQmvcZkDavYqOjfvvjXl7tpWLskmI7OEc'
+const url = 'https://fine-plum-crab-ring.cyclic.app'
 
-const bot = new Telegraf(BOT_TOKEN);
+const port = process.env.PORT || 3000;
 
-bot.on("text", ctx => ctx.reply("Hello"));
+const bot = new TelegramBot(TOKEN, {
+    webHook: {
+        port: port
+    }
+});
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
-// Start webhook via launch method (preferred)
-bot
-    .launch({ webhook: { domain, port } })
-    .then(() => console.log("Webhook bot listening on port", port)).catch(e=>console.log(e));
+const app = express();
+app.use(bodyParser.json());
+
+app.post(`/bot${TOKEN}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Hello, Welcome to my bot!');
+});
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'This is a help message');
+});
+
+// Deep linking
+bot.onText(/\/start (\w+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const deepLinkId = match[1]; // the captured "whatever"
+
+    // Use deepLinkId to decide what to do
+    bot.sendMessage(chatId, `Deep link clicked with ID: ${deepLinkId}`);
+});
+
+app.listen(port, () => {
+    console.log(`Express server is listening on ${port}`);
+});
