d71390d8630c797b933e53b1825c7b92234d4da4
max fabin <maxikfabin@gmail.com>
2023-06-23 00:38:49 +0300

    .

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 
 const BOT_TOKEN = '6143502881:AAEQmvcZkDavYqOjfvvjXl7tpWLskmI7OEc'
-const URL = 'https://fine-plum-crab-ring.cyclic.app'
-const express = require('express')
-const { Telegraf } = require('telegraf')
+const webhookDomain = 'https://fine-plum-crab-ring.cyclic.app'
+const port = 5000 || process.env.PORT
+import { Telegraf } from "telegraf";
 
-const PORT = process.env.PORT || 3000
+const bot = new Telegraf(BOT_TOKEN);
 
-const app = express()
-const bot = new Telegraf(BOT_TOKEN)
+bot.on("text", ctx => ctx.reply("Hello"));
 
-bot.start((ctx) => {
-    ctx.reply(`Вітаю! Спробуйте deep link: https://t.me/${ctx.botInfo.username}?start=test_payload`)
-})
-
-bot.command('start', (ctx) => {
-    const payload = ctx.message.text.split(' ')[1]
-    ctx.reply(`Deep link payload: ${payload}`)
-})
-
-// Тут ви можете додати інші обробники команд і повідомлень.
-
-bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`)
-app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`))
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// Start webhook via launch method (preferred)
+bot
+    .launch({ webhook: { domain: webhookDomain, port: port } })
+    .then(() => console.log("Webhook bot listening on port", port));
