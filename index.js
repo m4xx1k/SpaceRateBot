b2b4fc2cfa16a22d9c552ba1c115d1b8f8ff48ca
max fabin <maxikfabin@gmail.com>
2023-06-23 00:42:09 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const BOT_TOKEN = '6143502881:AAEQmvcZkDavYqOjfvvjXl7tpWLskmI7OEc'
-const webhookDomain = 'https://fine-plum-crab-ring.cyclic.app'
+const domain = 'https://fine-plum-crab-ring.cyclic.app'
 const port = 5000 || process.env.PORT
 import { Telegraf } from "telegraf";
 
@@ -10,5 +10,5 @@ bot.on("text", ctx => ctx.reply("Hello"));
 
 // Start webhook via launch method (preferred)
 bot
-    .launch({ webhook: { domain: webhookDomain, port: port } })
-    .then(() => console.log("Webhook bot listening on port", port));
+    .launch({ webhook: { domain, port } })
+    .then(() => console.log("Webhook bot listening on port", port)).catch(e=>console.log(e));
