055b0690f843d6823102787063946bdd1d06214f
max fabin <maxikfabin@gmail.com>
2023-08-03 19:45:09 +0300

    .

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://admin:admin@cluster0.usfqigj.mongodb.net/?retryWrites=true&w=majority',
+// const MONGO = 'mongodb+srv://admin:admin@cluster0.usfqigj.mongodb.net/?retryWrites=true&w=majority'
+const MONGO='mongodb://localhost:27017/test'
+mongoose.connect(MONGO,
     {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
